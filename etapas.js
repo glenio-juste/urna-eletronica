2327d3cf7e56b6ab734fa827f079b03fe65d535f
glenio-juste <glenio.juste@gmail.com>
2021-06-15 23:49:28 -0300

    Commit do projeto

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: '38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: '77222.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '55',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: '55.png', legenda: 'Prefeito'},
+                    {url: '55_2.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Zulano',
+                partido: 'QWERT',
+                vice: 'Zul',
+                fotos: [
+                    {url: '22.png', legenda: 'Prefeito'},
+                    {url: '22_2.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            }
+        ]
+    }
+
+]
